5627ce110ab21a6716411bbaafea325937d4e348
Kodjo ETSE <kodjodjidulaetse@gmail.com>
2022-12-08 09:22:53 -0800

    my first code line in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main - print a line of with puts function
+ * main -Print a line of with puts function
  *
- * return: 0
+ * Return: 0
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
-
 }
