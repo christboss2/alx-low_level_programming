ce0f6fdec87dcf3bbf39a657ee6a9034fe01daa1
Kodjo ETSE <kodjodjidulaetse@gmail.com>
2022-12-08 10:13:53 -0800

    my first code line in C with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Print a piece of code with the prinft command
+ *
+ * Return: 0
+*/
+int main(void)
+{
+
+	printf("with a proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
