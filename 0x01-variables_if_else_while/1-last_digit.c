c7980580b49c76b32900d74c1edac533e9c4f1b6
Kodjo ETSE <kodjodjidulaetse@gmail.com>
2022-12-10 04:03:04 -0800

    LAST DIGIT

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,12 +22,12 @@ int main(void)
 	}
 	if (x == 0)
 	{
-		printf("and is 0")
+		printf("and is 0");
 	}
-	if (x < 0 && != 0)
+	if (x < 0 && x != 0)
 	{
 		printf("and is less than 6 and not 0");
 	}
-	printf("\n")
+	printf("\n");
 	return (0);
 }
